91837f73565f0836424cf3ebedaf14e0d72aa697
krdlange <krdlange@gmail.com>
2023-05-11 23:21:35 +0200

    added styling to about component

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -25,52 +25,5 @@ export default {
 </script>
 
 <style>
-nav {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 80px;
-  background-color: rgba(255, 255, 255, 0);
-}
 
-.left-items {
-  display: flex;
-  align-items: center;
-}
-
-.brand-img img {
-  max-width: 100%;
-  height: auto;
-  max-height: 50px;
-  margin-right: 20px;
-}
-
-.right-items {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: rgba(255, 255, 255, 0);
-
-}
-/* 
-.logo img {
-  height: 50px;
-} */
-
-ul {
-  display: flex;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-
-li {
-  margin: 0 10px;
-}
-
-a {
-  text-decoration: none;
-  color: #333;
-  font-weight: bold;
-}
 </style>
