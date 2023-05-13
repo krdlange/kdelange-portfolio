03199bf278e037e230bd3b005b4c3ed02a7b7407
krdlange <krdlange@gmail.com>
2023-05-13 16:02:19 +0200

    fixed intro content

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,29 +1,138 @@
 <template>
   <nav>
-    <div class="left-items">
-      <div class="brand-img">
-        <img src="src/assets/logotransparent.png" alt="Logo">
+    <div class="brand-items">
+      <div class="left-items">
+        <div class="brand-img">
+          <img src="src/assets/logotransparent.png" alt="Logo">
+        </div>
+        <div class="brand-title">
+          <h1>Kriska de Lange</h1>
+        </div>
       </div>
-      <div class="brand-title">
-        <h1>Kriska de Lange</h1>
+
+      <div class="right-items">
+        <ul v-bind:class="{ 'menu': true, 'active': showMenu }">
+          <li><a href="#"><h4>About</h4></a></li>
+          <li><a href="#"><h4>Projects</h4></a></li>
+          <li><a href="#"><h4>Contact</h4></a></li>
+        </ul>
+        <div class="hamburger" v-on:click="showMenu = !showMenu" v-bind:class="{ 'close': showMenu }">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-    </div>
-    <div class="right-items">
-      <ul>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Component</a></li>
-      </ul>
     </div>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'Navigation'
+  data() {
+    return {
+      showMenu: false
+    }
+  }
 }
 </script>
 
 <style>
+/* Default styles for the menu */
 
+.menu {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.menu li {
+  margin: 10px 20px;
+}
+
+.menu a {
+  color: #000;
+  font-size: 1.2rem;
+  text-decoration: none;
+  color: #000;
+  border-bottom: 2px solid #000;
+  padding-bottom: 2px;
+}
+
+.brand-items {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.left-items {
+  display: flex;
+  align-items: center;
+}
+
+.brand-img img {
+  max-width: 100%;
+  height: auto;
+  max-height: 50px;
+  margin-right: 20px;
+}
+
+.right-items {
+  display: flex;
+  align-items: center;
+}
+
+/* Styles for the hamburger menu */
+.hamburger {
+  display: none;
+  cursor: pointer;
+}
+
+.hamburger span {
+  display: block;
+  width: 30px;
+  height: 2px;
+  margin-bottom: 5px;
+  background-color: #000;
+}
+
+.hamburger.close span:nth-child(1) {
+  transform: rotate(45deg) translate(5px, 5px);
+}
+
+.hamburger.close span:nth-child(2) {
+  opacity: 0;
+}
+
+.hamburger.close span:nth-child(3) {
+  transform: rotate(-45deg) translate(5px, -5px);
+}
+
+
+/* Media query for small devices */
+@media (max-width: 768px) {
+  .menu {
+    display: none;
+  }
+
+  .menu.active {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 100px;
+    left: 0;
+    height: 100vh;
+    width: 100%;
+    background-color: rgba(241, 223, 196, 0);
+    padding: 20px;
+    backdrop-filter: blur(20px); 
+  }
+
+  .hamburger {
+    display: block;
+  }
+}
 </style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,12 +1,39 @@
 <template>
-    <section id="projects">
-        <div class="project-1">
-            <h3>Project 1</h3>
+    <section class="projects-container">
+        <div class="title">
+             <h2>Projects</h2>
         </div>
-        <div class="project-2">
-            <h3>Project 2</h3>
+           <div class="arcadia-folder">
+            <div class="svg-container">
+               <svg class="folder-arcadia" width="409" height="287" viewBox="0 0 409 287" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M401 286.5H8.00001C3.85787 286.5 0.5 283.142 0.5 279V8.00001C0.5 3.85787 3.85787 0.5 8 0.5H160.5C164.642 0.5 168 3.85786 168 8V45.8125C168 50.5069 171.806 54.3125 176.5 54.3125H401C405.142 54.3125 408.5 57.6704 408.5 61.8125V279C408.5 283.142 405.142 286.5 401 286.5Z"
+                    fill="#F0F1EB" stroke="black" />
+            </svg> 
+            </div>
+            
 
-        </div>
+            <div class="project-container">
+             <div class="project-name">
+                <h3 class=""><sup>01</sup>Arcadia</h3>
+            </div>
+            <div class="project-description">
+                                <p>A web application that helps gamers find & discover new games, save them to
+                    their personal collection, and track their progress and ratings within the app.</p>
+            </div>   
+            </div>
+            
+
+            </div> 
+
+            <div class="rh-folder">
+                <svg width="409" height="287" viewBox="0 0 409 287" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M401 286.5H8.00001C3.85787 286.5 0.5 283.142 0.5 279V8.00001C0.5 3.85787 3.85787 0.5 8 0.5H203.5C207.642 0.5 211 3.85786 211 8V45.8125C211 50.5069 214.806 54.3125 219.5 54.3125H401C405.142 54.3125 408.5 57.6704 408.5 61.8125V279C408.5 283.142 405.142 286.5 401 286.5Z" fill="#F0F1EB" stroke="black" />
+</svg>
+            </div>
+        
+
     </section>
 </template>
 
@@ -16,4 +43,58 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+/* .projects-container {
+
+} */
+
+.arcadia-folder {
+    position: relative;
+}
+
+.folder-arcadia {
+    position: absolute;
+    left: 0;
+    top: 0;
+}
+
+.project-container {
+    display: flex;
+    position: absolute;
+    /* width: inherited; */
+}
+
+.project-name {
+    position: absolute;
+    width: inherited;
+    margin-left: 24px;
+    font-size: 1.2rem;
+    color: #333;
+}
+
+
+.project-description {
+    position: absolute;
+    top: 80px;
+    width: 300px;
+    margin-left: 24px;
+    font-size: 1.2rem;
+    color: #333;
+}
+
+sup {
+    font-size: 0.8rem;
+    margin-right: 8px;
+    vertical-align: super;
+}
+
+.rh-folder {
+    display: flex;
+}
+@media only screen and (min-width: 768px) {
+    .project-folder {
+        max-width: 500px;
+        width: 100%;
+    }
+}
+</style>
